b7e5bd17872910b9e3f0451d6970f1e0d7d624f2
SID <golangfanboysid@gmail.com>
2025-10-23 02:05:45 +0530

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart'; // Import ImagePicker
+import 'camera_page.dart';
 
 void main() {
   runApp(MyApp());
-  print("Starting up");
 }
 
-// stateless widget means it simply won't change its state over time
 class MyApp extends StatelessWidget {
-  int counter = 0;
-  bool isDarkMode = false;
-  bool isLightMode = true;
-  String? stack;
-
   MyApp({super.key});
 
   @override
@@ -22,7 +17,6 @@ class MyApp extends StatelessWidget {
       title: 'Secure qr',
       home: Scaffold(
         backgroundColor: Colors.lightBlue,
-
         appBar: AppBar(
           title: Text('Secure QR'),
           backgroundColor: Colors.deepPurpleAccent,
@@ -30,37 +24,56 @@ class MyApp extends StatelessWidget {
           actions: [IconButton(icon: Icon(Icons.settings), onPressed: () {})],
         ),
         body: Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              // this curves the corners
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            padding: EdgeInsets.all(25),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.person, size: 50, color: Colors.white),
-                Text(
-                  "Hello , Scan any qr with this app",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+                padding: EdgeInsets.all(25),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.person, size: 50, color: Colors.white),
+                    Text(
+                      "Hello, Scan any QR with this app",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CameraPage(initialImageSource: ImageSource.camera)),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple.shade100,
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  textStyle: TextStyle(fontSize: 18),
+                ),
+                child: Text('Go to Camera Page', style: TextStyle(color: Colors.white)),
+              ),
+            ],
           ),
         ),
       ),
